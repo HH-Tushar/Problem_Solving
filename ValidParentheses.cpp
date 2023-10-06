3dff3a8c84de40e30de171988cfa77369d249b74
Hasibul Hasan Tushar <tushar.cu.cse@gmail.com>
2023-10-07 03:18:30 +0600

    Valid Parentheses check done

---
+++ ValidParentheses.cpp
@@ -0,0 +1,73 @@
+// given string s="({})[]"
+// we need to validate if the order is correct.
+#include <iostream>
+#include <stack>
+using namespace std;
+
+bool isValid(string s)
+{
+    stack<char> checkEndingStack;
+    int i;
+    int length = s.size();
+
+    for (i = 0; i < length; i++)
+    {
+         if(checkEndingStack.empty()&& (s[i]==']'|| s[i]=='}'||s[i]==')'))
+    
+        {
+            return false;
+        }
+
+        if (s[i] == '(')
+        {
+            if (s[i + 1] == ')')
+            {
+                i++;
+            }
+            else
+            {
+                checkEndingStack.push(')');
+            }
+        }
+
+        else if (s[i] == '{')
+        {
+            if (s[i + 1] == '}')
+            {
+                i++;
+            }
+            else
+            {
+                checkEndingStack.push('}');
+            }
+        }
+        else if (s[i] == '[')
+        {
+            if (s[i + 1] == ']')
+            {
+                i++;
+            }
+            else
+            {
+                checkEndingStack.push(']');
+
+            }
+        }
+        else if (s[i] == checkEndingStack.top())
+        {
+            checkEndingStack.pop();
+        }
+
+        
+        
+    }
+    
+
+    return checkEndingStack.empty();
+}
+
+int main()
+{
+    cout << boolalpha << isValid("([({})])");
+    return 0;
+}
